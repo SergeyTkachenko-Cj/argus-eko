75657b2a91983e87d1f76a2aef40ed5d7abff2fc
CJTaylor <tka4inni@gmail.com>
2019-05-29 16:25:11 +0300

    Change: file.js & file.css/Add: 2 file.jpg & 1 file.tif

--- custom_js.js
+++ custom_js.js
@@ -2,3 +2,14 @@ window.onload = function() {
 	document.querySelector('#loader').style.display = 'none';
 	document.querySelector('#logo').style.animation = 'slide 1s forwards';
 }
+
+document.querySelector('body').onresize = function() {
+	// let lb = document.querySelector('#lightbox');
+
+	alert('bambi');
+
+	    // if (lb) {
+	    // 	lb.style.transform = rotateZ('180deg');
+	    // 	alert('bambi');
+	    // }
+};
