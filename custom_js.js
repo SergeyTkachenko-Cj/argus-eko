30b9cb5359d5938f243d04962233f038b9d99832
CJTaylor <tka4inni@gmail.com>
2019-05-30 16:18:18 +0300

    Change: file.js

--- custom_js.js
+++ custom_js.js
@@ -3,13 +3,47 @@ window.onload = function() {
 	document.querySelector('#logo').style.animation = 'slide 1s forwards';
 }
 
-document.querySelector('body').onresize = function() {
-	// let lb = document.querySelector('#lightbox');
+window.addEventListener('resize', bambi);
 
-	alert('bambi');
+function bambi() {
+	let lb = document.querySelector('#lightbox');
+	let nxt = document.querySelector('.lb-next');
+	let prv = document.querySelector('.lb-prev');
 
-	    // if (lb) {
-	    // 	lb.style.transform = rotateZ('180deg');
-	    // 	alert('bambi');
-	    // }
+	let img = document.querySelector('.lb-image');
+
+	    if (lb && lb.style.display === 'block' && img) {
+
+	    	let path = img.getAttribute('src');
+
+	    	// let bath = '[href=' +'"'+ path +'"' +']';
+
+	    	let bambi = document.querySelector("[href='path']");
+
+			// let lnk = document.querySelector("a[href=" +"'"+ path +"'"+ "]");
+
+			// let nwe = document.createElement('a');
+			// nwe.href=path;
+			// nwe.setAttribute('data-lightbox', 'jh-image-popup');
+			// nwe.click();
+
+	    	// nxt.style.display === 'none' ? prv_nxt() : nxt_prv();
+	    	// lb.style.setProperty('top', '50%', 'important');
+	    	// lb.style.transform = 'translateY(-50%)';
+
+	    	console.log(bambi);
+	    	// console.log(nwe.getAttribute('href') +' '+ nwe.getAttribute('data-lightbox'));
+
+	    	// path.click();
+	    }
+
+	// function nxt_prv() { 
+	// 	nxt.click();
+	//     prv.click();
+	// }
+
+	// function prv_nxt() { 
+	//     prv.click();
+	//     nxt.click();
+	// }
 };
