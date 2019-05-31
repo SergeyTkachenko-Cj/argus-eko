fb27ac46bcac0478712da70753c88a93385e4019
CJTaylor <tka4inni@gmail.com>
2019-05-31 13:52:58 +0300

    Change: file.css & file.js

--- custom_js.js
+++ custom_js.js
@@ -1,49 +1,19 @@
-window.onload = function() {
+window.onload = function() {	// site loading screen
 	document.querySelector('#loader').style.display = 'none';
 	document.querySelector('#logo').style.animation = 'slide 1s forwards';
 }
 
 window.addEventListener('resize', bambi);
 
-function bambi() {
+function bambi() {		// lightbox pop-up on-resize img screen fitting 
 	let lb = document.querySelector('#lightbox');
-	let nxt = document.querySelector('.lb-next');
-	let prv = document.querySelector('.lb-prev');
-
 	let img = document.querySelector('.lb-image');
 
-	    if (lb && lb.style.display === 'block' && img) {
+	    if (img) {
 
 	    	let path = img.getAttribute('src');
+	    	let bambi = document.querySelector("[href='"+path+"'][data-lightbox='jh-image-popup']");
 
-	    	// let bath = '[href=' +'"'+ path +'"' +']';
-
-	    	let bambi = document.querySelector("[href='path']");
-
-			// let lnk = document.querySelector("a[href=" +"'"+ path +"'"+ "]");
-
-			// let nwe = document.createElement('a');
-			// nwe.href=path;
-			// nwe.setAttribute('data-lightbox', 'jh-image-popup');
-			// nwe.click();
-
-	    	// nxt.style.display === 'none' ? prv_nxt() : nxt_prv();
-	    	// lb.style.setProperty('top', '50%', 'important');
-	    	// lb.style.transform = 'translateY(-50%)';
-
-	    	console.log(bambi);
-	    	// console.log(nwe.getAttribute('href') +' '+ nwe.getAttribute('data-lightbox'));
-
-	    	// path.click();
+	    	bambi.click();
 	    }
-
-	// function nxt_prv() { 
-	// 	nxt.click();
-	//     prv.click();
-	// }
-
-	// function prv_nxt() { 
-	//     prv.click();
-	//     nxt.click();
-	// }
 };
