ebf64fe4a154c1f7bf2ee56df40c2fe55b1eebe1
CJTaylor <tka4inni@gmail.com>
2019-06-04 17:03:37 +0300

    Change: file.js & file.css

--- custom_js.js
+++ custom_js.js
@@ -1,19 +1,29 @@
-window.onload = function() {	// site loading screen
-	document.querySelector('#loader').style.display = 'none';
-	document.querySelector('#logo').style.animation = 'slide 1s forwards';
+window.onload = function() {
+
+	(function resize_popup() {	// lightbox pop-up on-resize img screen fitting
+		window.addEventListener('resize', bambi);
+
+		function bambi() {		 
+			let lb = document.querySelector('#lightbox');
+			let img = document.querySelector('.lb-image');
+
+			    if (lb.style.display === 'block') {
+
+			    	let path = img.getAttribute('src');
+			    	let bambi = document.querySelector("[href='"+path+"'][data-lightbox='jh-image-popup']");
+
+			    	bambi.click();
+				}
+		};
+	})();
+
+	(function site_loader() {	// site loader screen animation
+		document.querySelector('#loader').style.display = 'none';
+		document.querySelector('#logo').style.animation = 'slide 1s forwards';
+	})();
+
+	(function autofill_off() {	// search input autofill off
+		var inpt = document.querySelectorAll('#search-searchword, #mod-search-searchword93');
+		inpt.forEach(function(i) { i.onfocus = i.setAttribute('autocomplete', 'off'); });
+	})();
 }
-
-window.addEventListener('resize', bambi);
-
-function bambi() {		// lightbox pop-up on-resize img screen fitting 
-	let lb = document.querySelector('#lightbox');
-	let img = document.querySelector('.lb-image');
-
-	    if (lb.style.display === 'block') {
-
-	    	let path = img.getAttribute('src');
-	    	let bambi = document.querySelector("[href='"+path+"'][data-lightbox='jh-image-popup']");
-
-	    	bambi.click();
-	    }
-};
